68fa6c0c4f603f15853b4db6c493c02f095c02b1
Piotr Komorowski <komorowski.piotr.91@gmail.com>
2019-01-11 14:04:40 +0100

    installed bcrypt js and created login user route

--- server/routes/api/user.js
+++ server/routes/api/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../../models/User');
+const bcrypt = require('bcryptjs');
 
 // @route   GET api/user/test
 // @desc    testing router
@@ -17,21 +18,53 @@ router.get('/test', (req, res) => {
 router.post('/register', (req, res) => {
     User.findOne({email: req.body.email})
         .then( user => {
+            //check if user is in db
             if (user) {
-                return res.status(404).json({msg: 'User with chosen email already exist'})
+                return res.status(400).json({msg: 'User with chosen email already exist'})
             } else {
+                //create new user
                 const newUser = new User({
                     name: req.body.name,
                     email: req.body.email,
                     password: req.body.password
                 });
 
-                newUser.save()
-                    .then(user => res.json(user))
-                    .catch(err => console.log(err))
+                //hash passowrd with bcryptjs
+                bcrypt.genSalt(10, (err, salt)=>{
+                    bcrypt.hash(newUser.password, salt, (err, hash)=>{
+                        newUser.password = hash;
+                        newUser.save()
+                            .then(user => res.json(user))
+                            .catch(err => console.log(err))
+                    })
+                });
+
             }
         })
 
 });
 
+// @route   POST api/user/login
+// @desc   login user on website if is already registered
+// @access  Public
+
+router.post('/login', (req, res) =>{
+    User.findOne({email: req.body.email})
+        .then( user => {
+            //check if user is in DB
+            if(!user){
+                return res.status(404).json({msg: 'invalid user'})
+            }
+
+            //if user is in db check password
+            bcrypt.compare(req.body.password, user.password)
+                .then( result => {
+                if(result){
+                    //will use jwt
+                    return res.json({msg: 'logged in'})
+                }else{
+                    return res.status(404).json({password: 'password incorrect'})                }
+            });
+        })
+} );
 module.exports = router;
